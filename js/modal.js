0ac5738be29589ed731e208d70682d3fe0f26303
shwed <krmelnik@gmail.com>
2026-07-19 13:02:05 +0300

    Refactor code structure for improved readability and maintainability

--- js/modal.js
+++ js/modal.js
@@ -28,3 +28,32 @@ document.addEventListener("keydown", event => {
     closeModal();
   }
 });
+
+const openMenuButton = document.querySelector("[data-menu-open]");
+const closeMenuButton = document.querySelector("[data-menu-close]");
+const mobileMenu = document.querySelector("[data-menu]");
+const mobileMenuLinks = mobileMenu.querySelectorAll("a");
+
+const openMenu = () => {
+  mobileMenu.classList.add("is-open");
+  openMenuButton.setAttribute("aria-expanded", "true");
+  document.body.classList.add("modal-open");
+  closeMenuButton.focus();
+};
+
+const closeMenu = () => {
+  mobileMenu.classList.remove("is-open");
+  openMenuButton.setAttribute("aria-expanded", "false");
+  document.body.classList.remove("modal-open");
+  openMenuButton.focus();
+};
+
+openMenuButton.addEventListener("click", openMenu);
+closeMenuButton.addEventListener("click", closeMenu);
+mobileMenuLinks.forEach(link => link.addEventListener("click", closeMenu));
+
+document.addEventListener("keydown", event => {
+  if (event.key === "Escape" && mobileMenu.classList.contains("is-open")) {
+    closeMenu();
+  }
+});
